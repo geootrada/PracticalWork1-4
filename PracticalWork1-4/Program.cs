5541bb469f52d043bd513bb7a1460dad270f658c
geootrada <geootrada@yandex.ru>
2023-12-07 13:46:28 +0300

    Добавьте файлы проекта.

---
+++ PracticalWork1-4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticalWork1_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Переменные для автомата по продаже газированной воды 
+            //Имеется три вида напитков и три разных вкуса, есть ограничение по росту покупателя
+            bool isThatCoin;
+            int coinNominal;
+            bool isHeightCorrect;
+            uint height;
+            byte numberDrink;
+            short airTemperature;
+            ushort typeDrink;
+            string nameDrink;
+            char nameButton;
+            float portionSize;
+            double totalVolumeWater;
+        }
+    }
+}
